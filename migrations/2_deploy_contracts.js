90e8d5eca9f0183acb1aee3e6996068a725a1122
djaciel <dj_roses@hotmail.com>
2021-05-07 01:08:04 -0500

    add the migration for the PisiSale smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const PisiToken = artifacts.require('PisiToken.sol');
+const PisiSale = artifacts.require('PisiSale.sol');
 
 module.exports = async function (deployer) {
+  const accounts = await web3.eth.getAccounts();
+
   await deployer.deploy(PisiToken, 10032019);
+  await deployer.deploy(PisiSale, 1, accounts[0], PisiToken.address);
+  const instance = await PisiToken.deployed();
+  instance.transfer(PisiSale.address, 10032019);
 };
