dcb0e72615e66e014139338268f3577abf0affda
djaciel <dj_roses@hotmail.com>
2021-05-06 23:04:03 -0500

    add migrations for truffle

--- contracts/PisiToken.sol
+++ contracts/PisiToken.sol
@@ -1,13 +1,11 @@
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract PISIToken is ERC20, ERC20Detailed {
-    constructor(uint256 initialSupply)
-        public
-        ERC20Detailed("Pisi Token", "PISI", 0)
-    {
+//import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract PISIToken is ERC20 {
+    constructor(uint256 initialSupply) public ERC20("Pisi Token", "PISI") {
         _mint(msg.sender, initialSupply);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,1 +1,5 @@
 const PisiToken = artifacts.require('PisiToken.sol');
+
+module.exports = async function (deployer) {
+  await deployer.deploy(PisiToken, 10032019);
+};
