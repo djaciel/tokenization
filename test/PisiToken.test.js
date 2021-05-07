cdc4f8e19034ab8e5738bac00a08ba543017e99e
djaciel <dj_roses@hotmail.com>
2021-05-07 00:41:44 -0500

    add more unit test for smart contract

--- test/PisiToken.test.js
+++ test/PisiToken.test.js
@@ -12,17 +12,41 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract('Pisi Token test', async (accounts) => {
+  const [deployerAccount, recipient, anotherAccount] = accounts;
+
   it('All tokens should be in my account', async () => {
     let instance = await PisiToken.deployed();
     let totalSupply = await instance.totalSupply();
-    // let balance = await instance.balanceOf(accounts[0]);
-    // assert.equal(
-    //   balance.valueOf(),
-    //   initialSupply.valueOf(),
-    //   'The balance was not the same'
-    // );
-    expect(await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(
-      totalSupply
+    expect(
+      instance.balanceOf(deployerAccount)
+    ).to.eventually.be.a.bignumber.equal(totalSupply);
+  });
+
+  it('Is possible to send tokens between accounts', async () => {
+    const sendTokens = 1;
+    let instance = await PisiToken.deployed();
+    let totalSupply = await instance.totalSupply();
+    expect(
+      instance.balanceOf(deployerAccount)
+    ).to.eventually.be.a.bignumber.equal(totalSupply);
+    expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+    expect(
+      instance.balanceOf(deployerAccount)
+    ).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+    expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(
+      new BN(sendTokens)
     );
+  });
+
+  it('Is not possible to send more tokens than available in total', async () => {
+    let instance = await PisiToken.deployed();
+    let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+
+    expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to
+      .eventually.be.rejected;
+
+    expect(
+      instance.balanceOf(deployerAccount)
+    ).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
   });
 });
